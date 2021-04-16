9f34c355ded9dc10184669456f02f09abf6fedb2
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 16:28:25 +0530

    changed installed apps

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'authentication',
-    'urlhandler',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'urlhandler',
     
 ]
 
