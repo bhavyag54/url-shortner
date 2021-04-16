dfe71e0299b1cd6b068455803e19079dca5f8ef1
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 17:09:33 +0530

    1

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -85,15 +85,16 @@ WSGI_APPLICATION = 'urlShortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
