eb720fc7ba3a3d59d1a8ee12df78b96d6da38392
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 17:36:59 +0530

    a

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import django_heroku
 import os
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,10 +87,16 @@ WSGI_APPLICATION = 'urlShortner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
