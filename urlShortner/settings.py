086bf1738bd94d0d0412e5d0baf2432fa1d06251
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 17:16:19 +0530

    debug

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$g3#mp6u32qzb*b(#zf3#0njm9hee7n9%gzw3s_jbv-dai%n0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'your-url-shortner.herokuapp.com',
