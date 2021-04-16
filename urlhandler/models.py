e2e1ecf0442bddc2bc294925e3e77bd5935c038c
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 18:02:35 +0530

    changed urlHandler models

--- urlhandler/models.py
+++ urlhandler/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class shorturl(models.Model):
-    original_url = models.URLField(blank = False)
+    original_url = models.CharField(blank=False, max_length=1000)
     short_url = models.CharField(blank=False, max_length=8)
     visits = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
