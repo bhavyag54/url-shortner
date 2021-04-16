06579bab43c5449ba2c3b9b017da25fed47ce062
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 15:38:03 +0530

    6

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 import django_heroku
+import gunicorn
+import whitenoise
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
